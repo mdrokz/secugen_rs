2c2f0ef8d2f8b0f0d36ba7bee51ca72546e4fbb0
mdrokz <mohammadmunshi@gmail.com>
2023-11-23 15:36:20 +0530

    feat: remove neon & prepare for publish

--- build.rs
+++ build.rs
@@ -15,8 +15,15 @@ fn main() {
         .write_to_file(&out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 
-    println!("cargo:rustc-link-search=native=/usr/local/lib");
     println!("cargo:rustc-link-lib=sgfplib");
-    println!("cargo:rustc-link-lib=jpeg");
-    println!("cargo:rustc-link-lib=stdc++");
+
+    if cfg!(target_os = "linux") {
+        println!("cargo:rustc-link-search=native=/usr/local/lib");
+        println!("cargo:rustc-link-lib=jpeg");
+        println!("cargo:rustc-link-lib=stdc++");
+    } else if cfg!(target_os = "windows") {
+        let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+        println!("cargo:rustc-link-search=native={}/libs/windows", manifest_dir);
+        println!("cargo:rustc-link-lib=sgfplib");
+    }
 }
