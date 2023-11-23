2387a1081c0e68393d48144759f03d15cff30d47
mdrokz <mohammadmunshi@gmail.com>
2023-11-23 15:51:51 +0530

    feat: push linux libs & update build.rs

--- build.rs
+++ build.rs
@@ -5,6 +5,8 @@ use std::env;
 fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").expect("No out dir found"));
 
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
     let bindings = bindgen::Builder::default()
         .clang_args(&["-x", "c++", "-std=c++11"])
         .header("./include/sgfplib.h")
@@ -18,11 +20,10 @@ fn main() {
     println!("cargo:rustc-link-lib=sgfplib");
 
     if cfg!(target_os = "linux") {
-        println!("cargo:rustc-link-search=native=/usr/local/lib");
+        println!("cargo:rustc-link-search=native={}/libs/linux", manifest_dir);
         println!("cargo:rustc-link-lib=jpeg");
         println!("cargo:rustc-link-lib=stdc++");
     } else if cfg!(target_os = "windows") {
-        let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
         println!("cargo:rustc-link-search=native={}/libs/windows", manifest_dir);
         println!("cargo:rustc-link-lib=sgfplib");
     }
