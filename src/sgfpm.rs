809f2942e70fbcbcf911313b3a66c9d80378ac29
mdrokz <mohammadmunshi@gmail.com>
2023-11-24 15:51:08 +0530

    fix: add platform specific code

--- src/sgfpm.rs
+++ src/sgfpm.rs
@@ -9,11 +9,21 @@ use crate::{
 };
 
 #[derive(Debug, Clone, Copy)]
+#[cfg(target_os = "linux")]
 pub struct DeviceInfo {
     pub image_width: u64,
     pub image_height: u64,
     pub com_speed: u64,
     pub com_port: u64,
+}
+
+#[derive(Debug, Clone, Copy)]
+#[cfg(target_os = "windows")]
+pub struct DeviceInfo {
+    pub image_width: u32,
+    pub image_height: u32,
+    pub com_speed: u32,
+    pub com_port: u32,
 }
 
 #[derive(Debug)]
@@ -47,8 +57,8 @@ impl FPM {
 
             let err = unsafe { SGFPM_GetImage(self.sgfpm as *mut c_void, buffer.as_mut_ptr()) };
 
-            if err != SGFDxErrorCode_SGFDX_ERROR_NONE.into() {
-                if err != SGFDxErrorCode_SGFDX_ERROR_WRONG_IMAGE.into() {
+            if err != SGFDxErrorCode_SGFDX_ERROR_NONE.try_into().unwrap() {
+                if err != SGFDxErrorCode_SGFDX_ERROR_WRONG_IMAGE.try_into().unwrap() {
                     return Err(format!("GetImage(): Failed : ErrorCode = {}", err));
                 }
             }
@@ -72,11 +82,11 @@ impl FPM {
                 self.sgfpm as *mut c_void,
                 min_template_1.as_mut_ptr(),
                 min_template_2.as_mut_ptr(),
-                secu_level.into(),
+                secu_level.try_into().unwrap(),
                 &mut matched as *mut i32,
             );
 
-            if err != SGFDxErrorCode_SGFDX_ERROR_NONE.into() {
+            if err != SGFDxErrorCode_SGFDX_ERROR_NONE.try_into().unwrap() {
                 return Err(format!("MatchTemplate(): Failed : ErrorCode = {}", err));
             }
         }
@@ -89,21 +99,32 @@ impl FPM {
         mut min_template_1: Vec<u8>,
         mut min_template_2: Vec<u8>,
     ) -> Result<u64, String> {
-        let mut score = 0;
+        #[cfg(target_os = "linux")]
+        let mut score: u64 = 0;
+    
+        #[cfg(target_os = "windows")]
+        let mut score: u32 = 0;
+    
+        #[cfg(target_os = "linux")]
+        let score_ptr = &mut score as *mut u64;
+    
+        #[cfg(target_os = "windows")]
+        let score_ptr = &mut score as *mut u32;
+
         unsafe {
             let err = SGFPM_GetMatchingScore(
                 self.sgfpm as *mut c_void,
                 min_template_1.as_mut_ptr(),
                 min_template_2.as_mut_ptr(),
-                &mut score as *mut u64,
+                score_ptr,
             );
 
-            if err != SGFDxErrorCode_SGFDX_ERROR_NONE.into() {
+            if err != SGFDxErrorCode_SGFDX_ERROR_NONE.try_into().unwrap() {
                 return Err(format!("MatchTemplate(): Failed : ErrorCode = {}", err));
             }
         }
 
-        Ok(score)
+        Ok(score.try_into().unwrap())
     }
 
     pub fn init_device(
@@ -128,21 +149,21 @@ impl FPM {
         unsafe {
             let err = SGFPM_Init(
                 self.sgfpm as *mut c_void,
-                SGFDxDeviceName_SG_DEV_AUTO.into(),
+                SGFDxDeviceName_SG_DEV_AUTO.try_into().unwrap(),
             );
 
-            if err != SGFDxErrorCode_SGFDX_ERROR_NONE.into() {
+            if err != SGFDxErrorCode_SGFDX_ERROR_NONE.try_into().unwrap() {
                 return Err(format!("Init: Failed : ErrorCode = {}", err));
             }
 
             let err = SGFPM_OpenDevice(self.sgfpm as *mut c_void, 0);
 
-            if err != SGFDxErrorCode_SGFDX_ERROR_NONE.into() {
+            if err != SGFDxErrorCode_SGFDX_ERROR_NONE.try_into().unwrap() {
                 return Err(format!("OpenDevice: Failed : ErrorCode = {}", err));
             }
 
             if let Some(v) = brightness {
-                SGFPM_SetBrightness(self.sgfpm as *mut c_void, v);
+                SGFPM_SetBrightness(self.sgfpm as *mut c_void, v.try_into().unwrap());
             } else {
                 SGFPM_SetBrightness(self.sgfpm as *mut c_void, 50);
             }
@@ -162,7 +183,7 @@ impl FPM {
             let mut p_info: MaybeUninit<SGDeviceInfoParam> = MaybeUninit::uninit();
             let err = SGFPM_GetDeviceInfo(self.sgfpm as *mut c_void, p_info.as_mut_ptr());
 
-            if err != SGFDxErrorCode_SGFDX_ERROR_NONE.into() {
+            if err != SGFDxErrorCode_SGFDX_ERROR_NONE.try_into().unwrap() {
                 return Err(format!("GetDeviceInfo: Failed : ErrorCode = {}", err));
             }
 
@@ -185,7 +206,7 @@ impl FPM {
         unsafe {
             let err = SGFPM_CloseDevice(self.sgfpm as *mut c_void);
 
-            if err != SGFDxErrorCode_SGFDX_ERROR_NONE.into() {
+            if err != SGFDxErrorCode_SGFDX_ERROR_NONE.try_into().unwrap() {
                 return Err(format!("CloseDevice: Failed : ErrorCode = {}", err));
             }
 
